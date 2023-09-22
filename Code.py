4eb6998e0c7aaa8a842c7c88eb269f4598dd8082
dheepaks33 <145792285+dheepaks33@users.noreply.github.com>
2023-09-22 23:01:58 +0530

    Code.py

--- Code.py
+++ Code.py
@@ -1,77 +1,66 @@
-import tkinter as tk # act as a GUI interface
-import random #to get random dice values
+import tkinter as tk # Importing Tkinter to use as GUI module
+import random # To generate dice values we use random func
 
-# Create the main window
+# Initialize the Tkinter app
 root = tk.Tk()
-root.title("Snake and Ladder Game")
+root.title("Snake and Ladder")
+
+# Define the game variables
+player1_position = 1
+player2_position = 1
+current_player = 1
 
 # Create a canvas to draw the game board
 canvas = tk.Canvas(root, width=400, height=400)
 canvas.pack()
 
-# Define the dimensions of the game board
-rows = 10
-columns = 10
-cell_size = 40
+# Define the snake and ladder positions
+snake_and_ladders = { 16: 6, 47: 26, 49: 11,56: 53, 62: 19, 64: 19, 87: 52, 93: 8, 95: 75, 98: 82 }
 
-# Create a list to store the player's position
-player_position = [0]
+# Create the game board
+for i in range(10):
+    for j in range(10):
+        if (i * 10 + j + 1) in snake_and_ladders:
+            cell_text = f"{i * 10 + j + 1} -> {snake_and_ladders[i * 10 + j + 1]}"
+        else:
+            cell_text = i * 10 + j + 1
+        canvas.create_rectangle(j * 40, i * 40, (j + 1) * 40, (i + 1) * 40, fill="white")
+        canvas.create_text((j + 0.5) * 40, (i + 0.5) * 40, text=cell_text)
 
-# Create a list of snakes and ladders positions
-snakes_and_ladders = {2: 38, 7: 14, 16: 6, 47: 26, 49: 11, 56: 53, 62: 19, 64: 13, 87: 37, 93: 69, 95: 75, 98: 78 }
-
-# Function to roll the dice and update the player's position
+# Function to roll the dice
 def roll_dice():
-    roll = random.randint(1, 6)
-    player_position[0] += roll
+    global current_player, player1_position, player2_position
+    dice_result = random.randint(1, 6)
+    if current_player == 1:
+        player1_position += dice_result
+        if player1_position in snake_and_ladders:
+            player1_position = snake_and_ladders[player1_position]
+    else:
+        player2_position += dice_result
+        if player2_position in snake_and_ladders:
+            player2_position = snake_and_ladders[player2_position]
 
-    # Check for snakes and ladders
-    if player_position[0] in snakes_and_ladders:
-        player_position[0] = snakes_and_ladders[player_position[0]]
+    current_player = 3 - current_player  # Switch player (1 -> 2, 2 -> 1)
 
-    # Update the player's position on the canvas
-    update_player_position()
+    # Update the player positions on the board
+    canvas.delete("players")
+    for pos, player in [(player1_position, "Player 1"), (player2_position, "Player 2")]:
+        row = (pos - 1) // 10
+        col = (pos - 1) % 10
+        x = (col + 0.5) * 40
+        y = (row + 0.5) * 40
+        canvas.create_oval(x - 15, y - 15, x + 15, y + 15, fill="red", tags="players")
+        canvas.create_text(x, y, text=player, tags="players")
 
-# Function to update the player's position on the canvas
-def update_player_position():
-    canvas.delete("player")
-    row, col = position_to_row_col(player_position[0])
-    x = col * cell_size + cell_size // 2
-    y = (9 - row) * cell_size + cell_size // 2
-    canvas.create_oval(x - 15, y - 15, x + 15, y + 15, fill="blue", tags="player")
-
-# Function to convert position to row and column
-def position_to_row_col(position):
-    row = position // columns
-    col = position % columns
-    if row % 2 == 1:
-        col = columns - 1 - col
-    return row, col
+    if player1_position >= 100:
+        canvas.create_text(200, 200, text="Player 1 wins!", font=("Helvetica", 20))
+        canvas.unbind("<Button-1>")
+    elif player2_position >= 100:
+        canvas.create_text(200, 200, text="Player 2 wins!", font=("Helvetica", 20))
+        canvas.unbind("<Button-1>")
 
 # Create a button to roll the dice
 roll_button = tk.Button(root, text="Roll Dice", command=roll_dice)
 roll_button.pack()
-
-# Create the game board
-for row in range(rows):
-    for col in range(columns):
-        x1 = col * cell_size
-        y1 = row * cell_size
-        x2 = x1 + cell_size
-        y2 = y1 + cell_size
-        canvas.create_rectangle(x1, y1, x2, y2, fill="lightgray")
-
-# Create snakes and ladders on the board
-for start, end in snakes_and_ladders.items():
-    row1, col1 = position_to_row_col(start)
-    row2, col2 = position_to_row_col(end)
-    x1 = col1 * cell_size + cell_size // 2
-    y1 = (9 - row1) * cell_size + cell_size // 2
-    x2 = col2 * cell_size + cell_size // 2
-    y2 = (9 - row2) * cell_size + cell_size // 2
-    canvas.create_line(x1, y1, x2, y2, fill="red", width=3)
-
-# Initialize the player's position
-update_player_position()
 
 root.mainloop()
